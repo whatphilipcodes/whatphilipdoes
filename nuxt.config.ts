7f54bca409bb17d9ddf56ff433717697a04e9b67
whatphilipcodes <gerdes.philip@gmail.com>
2023-10-13 18:09:28 +0200

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	typescript: {
+		strict: true,
+	},
+	modules: ['@nuxt/image'],
+	devtools: { enabled: true },
+})
