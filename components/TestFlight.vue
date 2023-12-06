24d7349b82a80fcf6fff701614543ae69b32a49a
whatphilipcodes <gerdes.philip@gmail.com>
2023-12-06 16:55:54 +0100

    msc: touch scroll blocker

--- components/TestFlight.vue
+++ components/TestFlight.vue
@@ -1,23 +1,41 @@
 <template>
 	<div id="top" class="w-screen h-[80vh] bg-mono-700">landing</div>
-	<div ref="trigger" class="w-full relative" />
+	<div ref="trigger" class="w-full relative">{{ msg }}</div>
 	<div id="slider" ref="container" class="w-screen h-screen bg-cinnabar-500">
 		slider
 	</div>
-	<div id="bottom" class="w-screen h-screen bg-mono-900">bottom</div>
+	<div id="bottom" class="sticky top-0 w-screen h-screen bg-mono-900">
+		bottom
+	</div>
 </template>
 
 <script setup lang="ts">
 import { useIntersectionObserver } from '#imports'
 import Lenis from '@studio-freight/lenis'
+import { stringifyQuery } from 'vue-router'
 
 const container = ref<HTMLElement | null>(null) // the element supposed to be scrolled to
 const trigger = ref<HTMLElement | null>(null)
 
+const msg = ref('booting')
+
 onMounted(() => {
-	const lenis = new Lenis({
-		smoothTouch: true,
-	})
+	msg.value = 'mounted'
+	let lockScrolling = true
+
+	// window.addEventListener(
+	// 	'wheel',
+	// 	(event) => {
+	// 		if (lockScrolling) {
+	// 			event.preventDefault()
+	// 		}
+	// 	},
+	// 	{
+	// 		passive: false,
+	// 	}
+	// )
+
+	const lenis = new Lenis()
 	function raf(time: any) {
 		lenis.raf(time)
 		requestAnimationFrame(raf)
@@ -42,7 +60,8 @@ onMounted(() => {
 			lock: true,
 			onComplete: () => {
 				console.log('scroll complete stopping instance')
-				lenis.stop()
+				// lenis.stop()
+				freezeTouchScrolling()
 			},
 		})
 	})
@@ -53,26 +72,22 @@ onMounted(() => {
 		lenis.start()
 	})
 })
+
+function freezeTouchScrolling() {
+	window.addEventListener(
+		'touchstart',
+		(event) => {
+			event.preventDefault()
+			const scrollY = 60
+			msg.value = `touchstart ${scrollY}`
+			// scroll back to current
+			window.scrollTo({
+				top: container.value?.offsetTop,
+			})
+		},
+		{
+			passive: false,
+		}
+	)
+}
 </script>
-
-<!-- <style lang="css">
-html.lenis {
-	height: auto;
-}
-
-.lenis.lenis-smooth {
-	scroll-behavior: auto;
-}
-
-.lenis.lenis-smooth [data-lenis-prevent] {
-	overscroll-behavior: contain;
-}
-
-.lenis.lenis-stopped {
-	overflow: hidden;
-}
-
-.lenis.lenis-scrolling iframe {
-	pointer-events: none;
-}
-</style> -->
