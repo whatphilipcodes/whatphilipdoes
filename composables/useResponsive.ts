3fd85a807b880866b09bc2c6a06f6588c5d26628
whatphilipcodes <gerdes.philip@gmail.com>
2023-12-07 11:03:18 +0100

    add: breakpoints for ts

---
+++ composables/useResponsive.ts
@@ -0,0 +1,8 @@
+export const useResponsive = () => {
+	// this is the breakpoints config -> needs to be synced with tailwind.config.js //todo make this dynamic
+	return useBreakpoints({
+		sm: '640px',
+		md: '768px',
+		lg: '1200px',
+	})
+}
