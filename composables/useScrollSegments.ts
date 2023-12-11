347e4f8cbcc31db6b5f012c2d4255492febd7f72
whatphilipcodes <gerdes.philip@gmail.com>
2023-12-11 20:04:12 +0100

    msc: refactor scrollSegmentsLoop function and add blockScrolling and enableScrolling methods

--- composables/useScrollSegments.ts
+++ composables/useScrollSegments.ts
@@ -5,13 +5,45 @@ export const useScrollSegments = () => {
 	// initialization
 	const activeSegement = ref(0)
 	toggleScrollTrigger(activeSegement.value)
-	const { stop } = useIntersectionObserver(
-		scrollSegmentTriggers[activeSegement.value].target,
-		([{ isIntersecting, target }]) => {
-			console.log('intersection observer', isIntersecting, target.id)
-		},
-		{
-			rootMargin: '-100px 0px 0px 0px',
-		}
-	)
+	scrollSegementsLoop()
+
+	// loop
+	function scrollSegementsLoop() {
+		const { stop } = useIntersectionObserver(
+			scrollSegmentTriggers[activeSegement.value].target,
+			([{ isIntersecting, target }]) => {
+				console.log('intersection observer', isIntersecting, target.id) // #rm
+				if (!isIntersecting) return
+				stop()
+				blockScrolling()
+				setTimeout(() => {
+					activeSegement.value++
+					toggleScrollTrigger(activeSegement.value)
+					enableScrolling()
+					scrollSegementsLoop()
+				}, 5000)
+			}
+		)
+	}
+
+	// methods
+	function blockScrolling() {
+		// #info: touchmove while allowing etc. btns without modification is not 100% reliable in blocking scrolling
+		window.addEventListener('touchstart', blockEvent, {
+			passive: false,
+		})
+		window.addEventListener('wheel', blockEvent, {
+			passive: false,
+		})
+	}
+
+	function enableScrolling() {
+		window.removeEventListener('touchstart', blockEvent)
+		window.removeEventListener('wheel', blockEvent)
+	}
+
+	function blockEvent(event: Event) {
+		console.log('blocking event')
+		event.preventDefault()
+	}
 }
