02f1bf3a18647c7230f934e2ef01232f7779c435
whatphilipcodes <gerdes.philip@gmail.com>
2023-12-15 00:21:54 +0100

    msc: simply using slug

--- pages/legal/[...slug].vue
+++ pages/legal/[...slug].vue
@@ -1,5 +1,5 @@
 <template>
 	<main class="col-span-full mb-56 lg:mb-32 lg:mt-24">
-		<ContentDoc path="/legal/imprint" />
+		<ContentDoc />
 	</main>
 </template>
--- pages/legal/privacy.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<main class="col-span-full mb-56 lg:mb-32 lg:mt-24">
-		<ContentDoc path="/legal/privacy" />
-	</main>
-</template>
-
-<script setup lang="ts"></script>
