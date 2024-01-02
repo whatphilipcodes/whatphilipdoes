4f94717f04541e5f5f2ebeb169689b4eca804724
whatphilipcodes <gerdes.philip@gmail.com>
2024-01-02 12:42:35 +0100

    add: todo

--- middleware/pageUpdater.global.ts
+++ middleware/pageUpdater.global.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware(async (to) => {
 	const { updateActivePage, updateActiveSegment } = useGlobalStore()
-	const { data } = await useAsyncData(() => queryContent(to.path).findOne())
+	const { data } = await useAsyncData(() => queryContent(to.path).findOne()) // #todo: prevent the useRoute() call nuxt-content uses internally
 	updateActivePage({ page: data.value?.meta.page })
 	updateActiveSegment({ title: data.value?.meta.segment })
 })
