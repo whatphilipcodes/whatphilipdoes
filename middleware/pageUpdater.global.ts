7d664858ac8d4045475787824ee73cd1e7f65b99
whatphilipcodes <gerdes.philip@gmail.com>
2024-01-04 14:04:06 +0100

    fx: prevent empty nav el

--- middleware/pageUpdater.global.ts
+++ middleware/pageUpdater.global.ts
@@ -1,6 +1,7 @@
 export default defineNuxtRouteMiddleware(async (to) => {
 	const { updateActivePage, updateActiveSegment } = useGlobalStore()
 	const { data } = await useAsyncData(() => queryContent(to.path).findOne()) // #todo: prevent the useRoute() call nuxt-content uses internally
+	if (!data.value) return
 	updateActivePage({ page: data.value?.meta.page })
 	updateActiveSegment({ title: data.value?.meta.segment })
 })
