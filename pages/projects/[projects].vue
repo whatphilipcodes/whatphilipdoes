4b6e51ca73f398578cf1c7c00cb7a8a48f5ed595
whatphilipcodes <gerdes.philip@gmail.com>
2024-01-10 02:01:37 +0100

    layout project card

--- pages/projects/[projects].vue
+++ pages/projects/[projects].vue
@@ -19,30 +19,34 @@
 				</div>
 				<div
 					data-info="spacer-animation"
-					class="w-full h-[40vh] flex flex-shrink-0"
+					class="flex flex-shrink-0 w-full h-[40vh]"
 				/>
 			</div>
 			<div data-info="meta" class="absolute top-0 flex flex-col w-full h-full">
-				<LayoutPadding data-info="text" class="w-full h-full">
+				<LayoutColumns data-info="text" class="w-full h-full auto-rows-min">
+					<div class="h-[60vh] lg:col-span-full" />
 					<div
 						data-info="overlay"
-						class="relative z-[20] col-span-full lg:col-span-6 lg:mt-auto"
+						class="relative z-[20] row-start-1 lg:row-start-auto col-span-3 lg:col-span-5 mt-4 md:mt-6 lg:mt-8"
 					>
-						<div data-info="title" class="bg-cinnabar-800">title</div>
+						<div data-info="title" class="bg-cinnabar-800">
+							this is a length test lalala land was a film muhahaha ich bin so
+							böse
+						</div>
 						<div data-info="tags" class="bg-cinnabar-600">tags</div>
 					</div>
 					<div
 						data-info="abstract"
-						class="col-span-full lg:col-span-6 mt-auto bg-cinnabar-400"
+						class="col-span-full lg:col-start-7 lg:col-span- mt-4 md:mt-6 lg:mt-8 bg-cinnabar-400"
 					>
 						Amet tellus eu sit tellus donec. Facilisi enim viverra amet
 						placerat. Ipsum molestie morbi dictum pharetra. Magnis pretium at
 						massa quam purus a laoreet.
 					</div>
-				</LayoutPadding>
+				</LayoutColumns>
 				<div
 					data-info="spacer-navbar"
-					class="w-full h-36 lg:hidden flex flex-shrink-0"
+					class="flex flex-shrink-0 w-full h-36 lg:hidden"
 				/>
 			</div>
 		</div>
