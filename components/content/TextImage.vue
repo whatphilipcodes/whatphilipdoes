7d868f9a1d8ee1f7b2083c81175360715999bfb8
whatphilipcodes <gerdes.philip@gmail.com>
2024-01-10 21:11:56 +0100

    fx: TextImage comp

--- components/content/TextImage.vue
+++ components/content/TextImage.vue
@@ -1,24 +1,60 @@
 <template>
-	<div class="flex flex-row flex-wrap col-span-full gap-4">
+	<div
+		data-info="sm-container"
+		class="md:hidden w-screen col-span-full justify-self-center"
+	>
+		<div
+			data-info="sm-carousel"
+			class="flex flex-row h-fit overflow-x-scroll gap-x-4 px-4"
+			v-if="props.imageArray.length > 1"
+		>
+			<div
+				v-for="(image, index) in props.imageArray"
+				:key="index"
+				:class="`h-[60vh]` + (image.wide ? ` aspect-[4/3]` : ` aspect-[3/4]`)"
+			>
+				<Image :src="image.src" :alt="image.alt" />
+			</div>
+		</div>
+		<div data-info="sm-single" class="flex h-fit overflow-hidden px-4" v-else>
+			<div class="h-[60vh] w-full">
+				<Image :src="props.imageArray[0].src" :alt="props.imageArray[0].alt" />
+			</div>
+		</div>
+		<LayoutColumns class="w-full h-fit my-4 md:my-6 lg:my-8">
+			<div v-if="description" class="col-span-full text-mono-300">
+				{{ props.description }}
+			</div>
+		</LayoutColumns>
+	</div>
+	<div
+		data-info="lg-grid"
+		class="hidden md:flex flex-row flex-wrap col-span-full h-fit gap-4"
+	>
 		<div
 			v-for="(image, index) in props.imageArray"
 			:key="index"
-			:class="`h-[40vh]` + (image.wide ? ` aspect-[4/3]` : ` aspect-[3/4]`)"
+			:class="
+				`flex w-fit h-[40vh]` + (image.wide ? ` aspect-[4/3]` : ` aspect-[3/4]`)
+			"
 		>
-			<NuxtPicture
-				format="webp"
+			<Image
 				:src="image.src"
 				:alt="image.alt"
-				:imgAttrs="{ class: 'w-full h-full object-cover' }"
+				class="w-full h-full object-cover"
 			/>
 		</div>
-		<div v-if="description" class="text-mono-300">
+		<div
+			v-if="description"
+			class="flex flex-1 min-w-[30vw] h-[40vh] items-start text-mono-300"
+		>
 			{{ props.description }}
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
+// props
 const props = defineProps({
 	imageArray: {
 		type: Array as PropType<{ src: string; alt?: string; wide?: boolean }[]>,
--- content/projects/entity.md
+++ content/projects/entity.md
@@ -10,6 +10,13 @@ imgs01:
     wide: true
   - src: /img/projects/entity/side-open.jpg
     alt: '02'
+  - src: /img/projects/entity/header-entity.jpg
+    alt: 'header'
+    wide: false
+imgs02:
+  - src: /img/projects/entity/header-entity.jpg
+    alt: 'header'
+    wide: true
 projectTags:
   - realtime differential-line-growth
   - external hardware communication
@@ -22,4 +29,6 @@ landing: true
 ::
 
 ::text-image{description="Cras sapien diam imperdiet massa scelerisque. Sodales vulputate fermentum eget massa. Mi pellentesque est interdum consequat habitant elit lectus. Fringilla gravida lectus rutrum scelerisque imperdiet elementum eu morbi. Tincidunt amet est quam cursus diam ipsum ultrices nulla. Nec tortor sed sed lectus aliquam ut. Sit ac eu id nunc pretium donec ultrices vel nisi." :imageArray="imgs01"}
+::
+::text-image{description="Tincidunt amet est quam cursus diam ipsum ultrices nulla. Nec tortor sed sed lectus aliquam ut. Sit ac eu id nunc pretium donec ultrices vel nisi." :imageArray="imgs02"}
 ::
