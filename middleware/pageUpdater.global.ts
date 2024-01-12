c4e540d0c7d0b9168ba1574d4770ecdab0ca1082
whatphilipcodes <gerdes.philip@gmail.com>
2024-01-12 12:46:17 +0100

    test: disable middleware pinia access

--- middleware/pageUpdater.global.ts
+++ middleware/pageUpdater.global.ts
@@ -1,10 +1,11 @@
 export default defineNuxtRouteMiddleware(async (to) => {
-	const { updateActivePage, updateActiveSegment } = useGlobalStore()
-	const { data } = await useAsyncData(() => queryContent(to.path).findOne()) // #todo: prevent the useRoute() call nuxt-content uses internally
-	if (!data.value) return
-	updateActivePage(data.value?.meta.page)
-	updateActiveSegment({
-		title: data.value?.meta.segment,
-		buttons: data.value?.meta.buttons ?? [],
-	})
+	// const { updateActivePage, updateActiveSegment } = useGlobalStore()
+	// const { data } = await useAsyncData(() => queryContent(to.path).findOne()) // #todo: prevent the useRoute() call nuxt-content uses internally
+	// if (!data.value) return
+	// updateActivePage(data.value?.meta.page)
+	// updateActiveSegment({
+	// 	title: data.value?.meta.segment,
+	// 	buttons: data.value?.meta.buttons ?? [],
+	// })
+	console.log('pageUpdater.global.ts')
 })
