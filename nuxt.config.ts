4e4e61981789621ec360c35a80ff324e18d85bd1
whatphilipcodes <gerdes.philip@gmail.com>
2024-01-12 13:56:41 +0100

    test: with generate instead

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	ssr: false,
+	spaLoadingTemplate: 'loading/suspense.html',
 	modules: [
 		'@pinia/nuxt',
 		'@nuxt/image',
