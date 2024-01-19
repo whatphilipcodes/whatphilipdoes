46d3aeb7007d3b0eac9b712253a1451dce412b0b
whatphilipcodes <gerdes.philip@gmail.com>
2024-01-19 23:41:09 +0100

    msc: restore legal

--- pages/legal/[legal].vue
+++ pages/legal/[legal].vue
@@ -1,9 +1,7 @@
 <template>
-	<ContentDoc v-slot="{ doc }">
-		<main class="col-span-full mb-56 lg:mb-32 lg:mt-24">
-			<ContentRenderer :value="doc" />
-		</main>
-	</ContentDoc>
+	<main class="col-span-full mb-56 lg:mb-32 lg:mt-24">
+		<ContentDoc />
+	</main>
 </template>
 
 <script setup lang="ts"></script>
