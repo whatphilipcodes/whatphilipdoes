c19e7b227f6588e3bafefeaf5d7a2f2c53fea0bc
whatphilipcodes <gerdes.philip@gmail.com>
2024-01-29 19:07:29 +0100

    msc: refactor text comp syntax

--- components/content/TextBlock.vue
+++ components/content/TextBlock.vue
@@ -2,15 +2,6 @@
 	<div
 		class="col-span-full md:col-start-4 md:col-end-9 lg:col-start-6 lg:col-end-13"
 	>
-		{{ props.text }}
+		<ContentSlot unwrap="*" />
 	</div>
 </template>
-
-<script setup lang="ts">
-const props = defineProps({
-	text: {
-		type: String,
-		required: true,
-	},
-})
-</script>
--- components/content/TextHighlight.vue
+++ components/content/TextHighlight.vue
@@ -2,15 +2,6 @@
 	<div
 		class="mt-8 col-span-full md:col-start-4 md:col-end-9 lg:col-start-6 lg:col-end-13 italic text-lg"
 	>
-		{{ props.text }}
+		<ContentSlot unwrap="*" />
 	</div>
 </template>
-
-<script setup lang="ts">
-const props = defineProps({
-	text: {
-		type: String,
-		required: true,
-	},
-})
-</script>
