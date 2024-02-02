19a7fcf15ec57e4cffb0a4e0847358989e314627
whatphilipcodes <gerdes.philip@gmail.com>
2024-02-02 18:04:15 +0100

    add: candy

--- app.vue
+++ app.vue
@@ -7,7 +7,13 @@
 	<div
 		data-info="background"
 		class="static-background fixed z-bottom h-lvh bottom-0 left-0 right-0"
-	></div>
+	>
+		<div
+			class="absolute right-0 max-md:top-0 lg:bottom-0 text-cinnabar-500 px-4 md:px-8 lg:px-32 mx-auto max-w-[1440px] py-8"
+		>
+			{{ new Date().getFullYear() }}, made in berlin.
+		</div>
+	</div>
 </template>
 
 <style scoped lang="css">
