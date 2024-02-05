51a6d52a679896223236a03f906e0e1ade077f41
whatphilipcodes <gerdes.philip@gmail.com>
2024-02-05 17:31:42 +0100

    rm: mt

--- components/content/TextHighlight.vue
+++ components/content/TextHighlight.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="mt-8 col-span-full md:col-start-4 md:col-end-9 lg:col-start-6 lg:col-end-13 italic text-lg"
+		class="col-span-full md:col-start-4 md:col-end-9 lg:col-start-6 lg:col-end-13 italic text-lg"
 	>
 		<ContentSlot unwrap="p" />
 	</div>
