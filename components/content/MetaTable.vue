b5419b4dbd0f5907105a768f704791b4132046ea
whatphilipcodes <gerdes.philip@gmail.com>
2024-02-06 12:08:22 +0100

    msc: move tables into main grid flow

--- components/content/MetaTable.vue
+++ components/content/MetaTable.vue
@@ -1,33 +1,31 @@
 <template>
-	<div class="col-span-full flex flex-col gap-8">
-		<div
-			class="col-span-full grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-4"
-			v-for="section in props.content"
-		>
-			<div class="text-mono-500">
-				{{ section.title.toLowerCase() }}
-			</div>
-			<template v-for="(row, rowdex) in section.rows">
-				<div
-					v-for="(col, coldex) in row.cols"
-					class="self-end"
-					:class="`col-start-${
-						coldex * cellDimensions.colSpan + 1
-					} md:col-start-${coldex * cellDimensions.colSpan + 2} lg:col-start-${
-						coldex * cellDimensions.colSpan + 3
-					} col-span-${cellDimensions.colSpan} row-start-${
-						rowdex * cellDimensions.rowSpan + 2
-					} md:row-start-${rowdex * cellDimensions.rowSpan + 1} row-span-${
-						cellDimensions.rowSpan
-					} odd:text-mono-500`"
-				>
-					<div v-if="Array.isArray(col)" v-for="item in col">
-						{{ item.toLowerCase() }}
-					</div>
-					<div v-else>{{ col.toLowerCase() }}</div>
-				</div>
-			</template>
+	<div
+		class="col-span-full grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12 gap-4"
+		v-for="section in props.content"
+	>
+		<div class="text-mono-500">
+			{{ section.title.toLowerCase() }}
 		</div>
+		<template v-for="(row, rowdex) in section.rows">
+			<div
+				v-for="(col, coldex) in row.cols"
+				class="self-end"
+				:class="`col-start-${
+					coldex * cellDimensions.colSpan + 1
+				} md:col-start-${coldex * cellDimensions.colSpan + 2} lg:col-start-${
+					coldex * cellDimensions.colSpan + 3
+				} col-span-${cellDimensions.colSpan} row-start-${
+					rowdex * cellDimensions.rowSpan + 2
+				} md:row-start-${rowdex * cellDimensions.rowSpan + 1} row-span-${
+					cellDimensions.rowSpan
+				} odd:text-mono-500`"
+			>
+				<div v-if="Array.isArray(col)" v-for="item in col">
+					{{ item.toLowerCase() }}
+				</div>
+				<div v-else>{{ col.toLowerCase() }}</div>
+			</div>
+		</template>
 	</div>
 </template>
 
