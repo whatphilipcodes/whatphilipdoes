a65d460d66b6393b12efbfbbd5169a21f856c1d1
whatphilipcodes <gerdes.philip@gmail.com>
2024-02-13 22:20:16 +0100

    fx: prevent blocking without content

--- middleware/pageUpdater.global.ts
+++ middleware/pageUpdater.global.ts
@@ -1,10 +1,17 @@
 export default defineNuxtRouteMiddleware(async (to) => {
 	const { updateActivePage, updateActiveSegment } = useGlobalStore()
-	const content = await queryContent(to.path).findOne() // useRoute warning caused by nuxt content
-	if (!content) return
-	updateActivePage(content.segments[0].page)
-	updateActiveSegment({
-		name: content.segments[0].name,
-		buttons: content.segments[0].buttons ?? [],
-	})
+	try {
+		const content = await queryContent(to.path).findOne() // useRoute warning caused by nuxt content
+		updateActivePage(content.segments[0].page)
+		updateActiveSegment({
+			name: content.segments[0].name,
+			buttons: content.segments[0].buttons ?? [],
+		})
+	} catch (error) {
+		console.error(
+			`The content page for the route ${to.fullPath} is missing. Please check the content directory.`,
+			error,
+		)
+		return
+	}
 })
