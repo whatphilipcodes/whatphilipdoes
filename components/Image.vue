64c5837f03b7bfcce76503996c265a5a722ee91d
whatphilipcodes <gerdes.philip@gmail.com>
2024-02-15 14:37:47 +0100

    fx: image load in smooth

--- components/Image.vue
+++ components/Image.vue
@@ -1,21 +1,33 @@
 <template>
-	<component
+	<div
+		ref="svgContainer"
 		v-if="isSvg"
-		class="h-full w-full object-cover"
-		:fontControlled="false"
-		filled
-		:is="svgComponent"
-	/>
-	<nuxt-img
-		v-else
-		:src="props.src"
-		:alt="props.alt"
-		class="h-full w-full bg-mono-900 object-cover"
-		:placeholder="img(props.src, { h: 40, q: 100, blur: 3 })"
-		format="webp"
-		sizes="480px md:720px lg:1024px"
-		:preload="true"
-	/>
+		class="h-full w-full opacity-0 transition-opacity duration-1000 ease-in-out"
+	>
+		<component
+			class="h-full w-full object-cover"
+			:fontControlled="false"
+			filled
+			:is="svgComponent"
+		/>
+	</div>
+	<div v-else class="relative h-full w-full">
+		<nuxt-img
+			ref="image"
+			:src="props.src"
+			:alt="props.alt"
+			class="h-full w-full bg-mono-900 object-cover"
+			:placeholder="img(props.src, { h: 20, q: 100 })"
+			:preload="true"
+			format="webp"
+			sizes="480px md:720px lg:1024px"
+			@load="unblur"
+		/>
+		<div
+			ref="blurOverlay"
+			class="absolute top-0 h-full w-full backdrop-blur-xl transition-opacity duration-1000 ease-in-out"
+		></div>
+	</div>
 </template>
 
 <script setup lang="ts">
@@ -29,8 +41,19 @@ const props = defineProps({
 		required: false,
 	},
 })
+
 const img = useImage()
 const isSvg = computed(() => props.src.endsWith('.svg'))
 const svgComponent = props.src.replace('.svg', '')
 // note: using q100 for placeholder to make it look better since rendering happens at the build step
+
+const blurOverlay = ref<HTMLElement>()
+function unblur() {
+	blurOverlay.value?.classList.add('opacity-0')
+}
+
+const svgContainer = ref<HTMLElement>()
+watchOnce(svgContainer, () => {
+	svgContainer.value?.classList.add('opacity-100')
+})
 </script>
