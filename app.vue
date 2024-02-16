651ee0e38909a6b136e2cdd0c8587d77b8e56f7b
whatphilipcodes <gerdes.philip@gmail.com>
2024-02-16 01:11:48 +0100

    add: boot animation

--- app.vue
+++ app.vue
@@ -7,16 +7,19 @@
 		<Nav />
 		<NuxtPage />
 	</div>
-	<div
-		class="absolute top-0 z-[80] h-screen w-screen bg-mono-950 transition-opacity duration-500"
-		:class="{ 'opacity-0': !isLoading }"
-	/>
+	<Loading v-if="loaderMounted" :class="{ 'opacity-0': !isLoading }" />
 </template>
 
 <script lang="ts" setup>
 const isLoading = ref(true)
+const loaderMounted = ref(true)
 onMounted(() => {
-	isLoading.value = false
+	setTimeout(() => {
+		isLoading.value = false
+	}, 300)
+	setTimeout(() => {
+		loaderMounted.value = false
+	}, 1000)
 })
 </script>
 
@@ -28,7 +31,7 @@ onMounted(() => {
 .fade-enter-active,
 .fade-leave-active {
 	opacity: 1;
-	transition: all 0.5s;
+	transition: all 0.3s;
 }
 .fade-enter-from,
 .fade-leave-to {
---
+++ components/Loading.vue
@@ -0,0 +1,24 @@
+<template>
+	<div
+		class="loading-gradient pointer-events-none fixed bottom-0 left-0 right-0 top-0 z-[80] h-screen w-screen transition-opacity duration-500"
+	/>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.loading-gradient {
+	background: linear-gradient(to bottom, #180602 40%, #300d03 60%, #0d0d0d);
+	background-size: 100% 200%;
+	animation: gradient 1s linear infinite;
+}
+
+@keyframes gradient {
+	0% {
+		background-position: 0% 0%;
+	}
+	100% {
+		background-position: 0% 200%;
+	}
+}
+</style>
