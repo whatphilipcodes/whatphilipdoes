c7876aeddb6e5c05d814d7e1a086be9fb557d4d3
whatphilipcodes <gerdes.philip@gmail.com>
2024-02-17 17:21:16 +0100

    msc: better loading

--- components/Loading.vue
+++ components/Loading.vue
@@ -1,5 +1,8 @@
 <template>
 	<div
+		class="absolute top-0 z-nav h-16 w-full bg-gradient-to-b from-mono-950 md:hidden"
+	></div>
+	<div
 		class="loading-gradient pointer-events-none fixed bottom-0 left-0 right-0 top-0 z-[80] h-screen w-screen transition-opacity duration-500"
 	/>
 </template>
@@ -8,7 +11,15 @@
 
 <style scoped>
 .loading-gradient {
-	background: linear-gradient(to bottom, #180602 40%, #300d03 60%, #0d0d0d);
+	background: linear-gradient(
+		to bottom,
+		#0d0d0d 0%,
+		#300d03 20%,
+		#0d0d0d 40%,
+		#0d0d0d 60%,
+		#300d03 80%,
+		#0d0d0d 100%
+	);
 	background-size: 100% 200%;
 	animation: gradient 1s linear infinite;
 }
