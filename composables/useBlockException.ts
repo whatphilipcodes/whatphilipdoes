920f77a93177059f21f20d7fcab7cb193e76aa18
whatphilipcodes <gerdes.philip@gmail.com>
2024-02-21 12:13:53 +0100

    fx: class instancing to prevent side effects

--- composables/useBlockException.ts
+++ composables/useBlockException.ts
@@ -1,4 +1,27 @@
-export const useBlockException = (event: Event) => {
-	const target = event.target as HTMLElement
-	if (!target?.classList.contains('persistent-default')) event.preventDefault()
+export class BlockExceptionHandler {
+	private isActive: boolean = true
+	private block: (event: Event) => void
+
+	constructor() {
+		this.block = this.blockHandler.bind(this) // Bind `this` context
+	}
+
+	private blockHandler(event: Event) {
+		const target = event.target as HTMLElement
+		if (this.isActive && !target?.classList.contains('persistent-default')) {
+			event.preventDefault()
+		}
+	}
+
+	attachEvent(eventName: string, target: EventTarget, passive = false) {
+		target.addEventListener(eventName, this.block, { passive })
+	}
+
+	detachEvent(eventName: string, target: EventTarget) {
+		target.removeEventListener(eventName, this.block)
+	}
+
+	deactivate() {
+		this.isActive = false
+	}
 }
