cc36e12aa7a82dea8addb3794402f9f6fc735363
whatphilipcodes <gerdes.philip@gmail.com>
2024-02-28 19:28:17 +0100

    fx: spacing

--- pages/legal/[legal].vue
+++ pages/legal/[legal].vue
@@ -1,14 +1,9 @@
 <template>
 	<TransitionWrapper>
 		<div lang="de">
-			<div datainfo="spacer-header" class="h-36" />
 			<LayoutColumns>
 				<ContentDoc v-slot="{ doc }">
-					<ContentRenderer
-						:value="doc"
-						tag="div"
-						class="col-span-full lg:mt-32"
-					/>
+					<ContentRenderer :value="doc" tag="div" class="col-span-full mt-36" />
 					<div class="h-8" />
 					<Footer />
 				</ContentDoc>
