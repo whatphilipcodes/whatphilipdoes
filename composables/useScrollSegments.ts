68831cfc85fe02dbbc18fe123c18916cff719331
whatphilipcodes <gerdes.philip@gmail.com>
2024-03-06 18:09:56 +0100

    fx: transition flash in

--- composables/useScrollSegments.ts
+++ composables/useScrollSegments.ts
@@ -12,8 +12,9 @@ export const useScrollSegments = () => {
 
 	//controller
 	onMounted(() => {
-		watchOnce(currentSegments, (value: any) => {
-			updateActiveSegment(value[0]?.segment)
+		watchOnce(currentSegments, () => {
+			if (y.value > 0) return // bandaid fix -> todo: proper handling
+			updateActiveSegment(currentSegments.value[0]?.segment)
 		})
 		cbUnwatch = watch(y, () => {
 			const segment = currentSegments.value[0]?.segment
@@ -37,7 +38,7 @@ export const useScrollSegments = () => {
 	const currentSegments = computed(() => {
 		const center = y.value + window.innerHeight / 2
 		const activeSegments = scrollSegments.filter(
-			(segment) => segment.enter <= center && segment.exit >= center
+			(segment) => segment.enter <= center && segment.exit >= center,
 		)
 		return activeSegments
 	})
