c912b2f3efce20310419cbc092e6cea0b429b85a
whatphilipcodes <gerdes.philip@gmail.com>
2025-11-03 02:43:46 +0100

    fix: robots & favicon

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,7 +2,8 @@ import type { APIRoute } from 'astro';
 
 const getRobotsTxt = (sitemapURL: URL) => `\
 User-agent: *
-Allow: /
+Disallow: /downloads/
+Disallow: /legal/
 
 Sitemap: ${sitemapURL.href}
 `;
