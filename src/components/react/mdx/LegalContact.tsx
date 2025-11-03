0c1859d3fb2434bdda69013089b2b6ddefed3435
whatphilipcodes <gerdes.philip@gmail.com>
2025-11-03 02:39:42 +0100

    feat: BaseHead & fix: formatting issue

--- src/components/react/mdx/LegalContact.tsx
+++ src/components/react/mdx/LegalContact.tsx
@@ -3,7 +3,7 @@ type LegalContactProps = {
 };
 
 const LegalContact = ({ children }: LegalContactProps) => {
-	return <div>{children}</div>;
+	return <div className="text-cinnabar-500">{children}</div>;
 };
 
 export default LegalContact;
