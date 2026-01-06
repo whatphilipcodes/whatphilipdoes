dc378466db8711181230ac0b25fd98104ca160b4
whatphilipcodes <gerdes.philip@gmail.com>
2026-01-06 13:53:47 +0100

    fix: minor stylistic changes

--- src/components/react/NavBar.tsx
+++ src/components/react/NavBar.tsx
@@ -1,11 +1,14 @@
 const NavBar = () => {
 	return (
-		<nav className='fixed bottom-10 left-1/2 -translate-x-1/2 text-mono-500 text-lg z-1'>
+		<nav className='fixed bottom-10 left-1/2 -translate-x-1/2 text-mono-500 text-lg z-50'>
 			<div className='flex flex-row place-items-center h-12 gap-6'>
-				<a className='border border-mono-800 rounded-lg px-5 py-2' href='/'>
+				<a
+					className='rounded-lg px-5 py-2 border border-mono-900 bg-mono-950/80 backdrop-blur-md'
+					href='/'
+				>
 					wp
 				</a>
-				<div className='flex flex-row border border-mono-800 text-mono-800 rounded-lg px-5 py-2 gap-8'>
+				<div className='flex flex-row border text-mono-800 rounded-lg px-5 py-2 gap-8 border-mono-900 bg-mono-950/80 backdrop-blur-md'>
 					<div className='flex flex-row gap-2'>
 						<span>/</span>
 						<a href='#does'>does</a>
@@ -20,7 +23,7 @@ const NavBar = () => {
 					</div>
 				</div>
 				<a
-					className='border border-mono-800 rounded-lg px-5 py-2'
+					className='border rounded-lg px-5 py-2 border-mono-900 bg-mono-950/80 backdrop-blur-md'
 					href='#contact'
 				>
 					contact
