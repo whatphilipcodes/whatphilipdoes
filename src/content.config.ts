36bbee3dc14e1e10c4077bafd818437d02e5c868
whatphilipcodes <gerdes.philip@gmail.com>
2026-06-12 11:36:10 +0200

    refactor: routing

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,7 +6,10 @@ const baseSchema = z.object({
 	draft: z.boolean().optional(),
 	title: z.string(),
 	description: z.string(),
+	previewImage: z.string().optional(),
 });
+
+type HeadProps = z.infer<typeof baseSchema>;
 
 const projectSchema = baseSchema.extend({
 	tags: z.array(z.string()),
@@ -15,6 +18,11 @@ const projectSchema = baseSchema.extend({
 const basePattern = '**/[^_]*.{md,mdx}';
 
 // --- Collections --- //
+
+const nav = defineCollection({
+	loader: glob({ base: 'content/nav', pattern: basePattern }),
+	schema: baseSchema,
+});
 
 const built = defineCollection({
 	loader: glob({ base: 'content/built', pattern: basePattern }),
@@ -26,4 +34,5 @@ const legal = defineCollection({
 	schema: baseSchema,
 });
 
-export const collections = { built, legal };
+export const collections = { nav, built, legal };
+export type { HeadProps };
