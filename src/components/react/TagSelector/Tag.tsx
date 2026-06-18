6d80afe48e712756354b42a454385dbc1de6a799
whatphilipcodes <gerdes.philip@gmail.com>
2026-06-18 12:17:17 +0200

    fix: missing disabled tag

--- src/components/react/TagSelector/Tag.tsx
+++ src/components/react/TagSelector/Tag.tsx
@@ -8,11 +8,13 @@ interface TagProps {
 export default function Tag({ tag, href, active, disabled }: TagProps) {
 	return disabled ? (
 		<div
-			className={`rounded-md px-3 ring ${active ? 'text-mono-500 ring-mono-200' : 'ring-mono-800'}}`}
-		></div>
+			className={`rounded-md px-3 ring ${active ? 'ring-mono-200' : 'text-mono-500 ring-mono-800'}`}
+		>
+			{tag}
+		</div>
 	) : (
 		<a
-			className={`rounded-md px-3 ring ${active ? 'text-mono-500 ring-mono-200' : 'ring-mono-800'}}`}
+			className={`rounded-md px-3 ring ${active ? 'ring-mono-200' : 'text-mono-500 ring-mono-800'}`}
 			href={href}
 		>
 			{tag}
