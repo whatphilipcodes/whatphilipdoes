81752ca24075534b58dc1a12834d812cb0e7d2df
whatphilipcodes <gerdes.philip@gmail.com>
2026-07-19 16:13:34 +0200

    feat: cookie setup draft

--- src/store/menuStore.ts
+++ src/store/menuStore.ts
@@ -2,5 +2,6 @@ import type { ActionAnchorProps } from '@react/ActionAnchor';
 import { atom, map } from 'nanostores';
 
 export const $currentActionStore = map<Partial<ActionAnchorProps>>({});
+export const $cookies = atom<boolean>(false);
 export const $locale = atom<'en-GB' | 'de-DE'>('en-GB');
 export const $theme = atom<'dark' | 'light'>('dark');
